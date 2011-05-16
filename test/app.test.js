8f6149746c8e62a974367cde24d291a50955a7d6
Fábio Cerqueira <steelday@gmail.com>
2011-05-16 20:31:25 -0300

    adicionado novos testes
    usando expresso: npm install expresso

--- test/app.test.js
+++ test/app.test.js
@@ -10,12 +10,38 @@ var app = require('../app')
 
 
 module.exports = {
-  'GET /': function(){
+  'GET /': function() {
     assert.response(app,
       { url: '/' },
+      { status: 302 });
+  },
+
+  'GET /braziljs/twitter/': function() {
+    assert.response(app,
+      { url: '/braziljs/twitter/' },
       { status: 200, headers: { 'Content-Type': 'text/html; charset=utf-8' }},
-      function(res){
-        assert.includes(res.body, '<title>Express</title>');
+        function(res) {
+            assert.includes(res.body, '<title>twitter</title>');
       });
-  }
+  },
+
+  'GET /braziljs/delicious/': function() {
+    assert.response(app,
+      { url: '/braziljs/delicious/' },
+      { status: 200, headers: { 'Content-Type': 'text/html; charset=utf-8' }},
+        function(res) {
+            assert.includes(res.body, '<title>delicious</title>');
+      });
+  },
+
+  'GET /braziljs/flickr/': function() {
+    assert.response(app,
+      { url: '/braziljs/flickr/' },
+      { status: 200, headers: { 'Content-Type': 'text/html; charset=utf-8' }},
+        function(res) {
+            assert.includes(res.body, '<title>flickr</title>');
+      });
+  },
+
+
 };
