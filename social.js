5683de5159303a566409bd1bfddfeacf1a5a40e3
Fábio Cerqueira <cerqueirasfabio@gmail.com>
2011-06-13 02:01:09 -0300

    removido evil eval

--- social.js
+++ social.js
@@ -12,7 +12,7 @@ social.factory = function(snetwork) {
     // check if snetwork in social.INSTALLEDS
     if (social.INSTALLEDS.indexOf(snetwork) == -1)
         throw snetwork + ' Does not exists!';
-    var network = eval('social.' + snetwork);
+    var network = social[snetwork];
     return new network();
 };
 
